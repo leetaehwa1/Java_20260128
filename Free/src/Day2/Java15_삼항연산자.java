3dd157327fa39117f7c60cb70f6d862d712e9dec
tj-bu-708-10 <tj-bu-708-10@DESKTOP-62OGMPR>
2026-01-29 14:39:05 +0900

    2026.01.29

--- Free/src/Day2/Java15_삼항연산자.java
+++ Free/src/Day2/Java15_삼항연산자.java
@@ -12,6 +12,7 @@ public class Java15_삼항연산자 {
 		String grade = score >= 60 ? "P" :"F"; //이 한 줄과 밑에 if문은 같은 코딩.
 //		String grade;			//true값  //false값
 //		
+		//자바에서는 중괄호 안에 한 줄만 들어가는 경우에 한해서 중괄호 생략가능
 //		if(score >= 60) {
 //			grade = "P";
 //		} else {
